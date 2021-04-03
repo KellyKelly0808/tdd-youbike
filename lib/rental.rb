b8c9b42fc128bb584eb0b2ee3b6a8aa729791a35
kellykelly0808 <kellykelly0808@bitbucket.org>
2021-04-03 16:31:32 +0800

    it is ok

---
+++ lib/rental.rb
@@ -0,0 +1,15 @@
+class Rental
+    def initialize(user)
+        @user = user
+    end
+
+    def rent(bike)
+    end
+
+    def return(bike)
+    end
+
+    def charge
+        50
+    end
+end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,5 +1,22 @@
 require 'bike'
+require 'timecop'
+require'rental'
 
 RSpec.describe Bike do
+  context'非會員租借' do
+        it  " 4 小時內，費率為每 30 分鐘 10 元 " do
+          # Arrange
+          user = User.new
+          bike = Bike.new
+          rental = Rental.new(user)
 
-end
+          #Act
+          rental.rent(bike)
+           Timecop.travel( 65 * 60 ) #時間前進65分鐘
+          rental.return(bike)
+          # Assert
+          expect(rental.charge).to be 50
+        end
+   end
+
+  end
