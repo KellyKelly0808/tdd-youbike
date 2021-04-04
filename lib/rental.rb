e829c7687829dfde540fbc1bc76c08938270cd58
kellykelly0808 <kellykelly0808@bitbucket.org>
2021-04-04 17:30:47 +0800

    practice done

--- lib/rental.rb
+++ lib/rental.rb
@@ -3,13 +3,33 @@ class Rental
         @user = user
     end
 
-    def rent(bike)
+    def add(bike)
+        bike.rent!
+        @bike = bike
     end
 
-    def return(bike)
+    def remove(bike)
+        bike.return!
     end
 
     def charge
-        50
+        mins = @bike.rental_duration_mins
+
+        if @user.member?
+
+
+
+        else
+                # 4 小時內，費率為每 30 分鐘 10 元
+                case mins
+                when 0..240
+                (mins / 30).ceil * 10
+                else
+                    80 + ((mins - 240) / 30).ceil * 20
+                end
+
+        end
+        
+       
     end
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -11,12 +11,35 @@ RSpec.describe Bike do
           rental = Rental.new(user)
 
           #Act
-          rental.rent(bike)
+          rental.add(bike)
            Timecop.travel( 65 * 60 ) #時間前進65分鐘
-          rental.return(bike)
+          rental.remove(bike)
+
           # Assert
-          expect(rental.charge).to be 50
+          expect(rental.charge).to be 30
         end
+
+    it "超過 4 小時，但於 8 小時內還車，第 4~8 小時費率為每 30 分鐘 20 元" do
+              
+          # Arrange
+          user = User.new
+          bike = Bike.new
+          rental = Rental.new(user)
+
+          #Act
+          rental.add(bike)
+            Timecop.travel( 305 * 60 ) #時間前進5小時又5分鐘(305分鐘)
+          rental.remove(bike)
+
+          # Assert
+          expect(rental.charge).to be 140
+    
+    end
+
+
+
+
+
    end
 
   end
